358c14328befe5e091dc7c88bffbd80a73d17fc8
Maciej Waloszczyk <110481797+maciejwaloszczyk@users.noreply.github.com>
2024-11-12 18:15:31 +0100

    Update z3.cpp

--- Logika/z3.cpp
+++ Logika/z3.cpp
@@ -1,17 +1,12 @@
+//Maciej Waloszczyk - ITE-1sem WIMiIP 2024
 #include <iostream>
 using namespace std;
-
-// Funkcja dla operacji implikacji
 bool implikacja(bool p, bool q) {
     return !p || q;
 }
-
-// Funkcja dla operacji równoważności
 bool rownowaznosc(bool p, bool q) {
     return p == q;
 }
-
-// Funkcja obliczająca wartości zdań złożonych na podstawie p, q, r
 void ocena_zdan(bool p, bool q, bool r) {
     cout << "a) p ∧ q: " << (p && q) << endl;
     cout << "b) p ∨ q: " << (p || q) << endl;
@@ -21,13 +16,10 @@ void ocena_zdan(bool p, bool q, bool r) {
     cout << "f) [(p → r) ∨ ¬q] ↔ [p → (r ∧ ¬q)]: " << rownowaznosc((implikacja(p, r) || !q), implikacja(p, (r && !q))) << endl;
     cout << "g) [(¬r ∨ q) ∨ ¬(q ∧ r)] → [¬(q → p)]: " << implikacja((!r || q) || !(q && r), !implikacja(q, p)) << endl;
 }
-
 int main() {
-    // Przykład użycia
     bool p = true;
     bool q = false;
     bool r = true;
-
     ocena_zdan(p, q, r);
 
     return 0;
