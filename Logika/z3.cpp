37d6e3c4fa321a7471a761b68cdbfef3e8991b00
Maciej Waloszczyk <110481797+maciejwaloszczyk@users.noreply.github.com>
2024-11-14 23:23:08 +0100

    Update z3.cpp

--- Logika/z3.cpp
+++ Logika/z3.cpp
@@ -1,12 +1,15 @@
 //Maciej Waloszczyk - ITE-1sem WIMiIP 2024
 #include <iostream>
 using namespace std;
+
 bool implikacja(bool p, bool q) {
     return !p || q;
 }
+
 bool rownowaznosc(bool p, bool q) {
     return p == q;
 }
+
 void ocena_zdan(bool p, bool q, bool r) {
     cout << "a) p ∧ q: " << (p && q) << endl;
     cout << "b) p ∨ q: " << (p || q) << endl;
@@ -16,10 +19,13 @@ void ocena_zdan(bool p, bool q, bool r) {
     cout << "f) [(p → r) ∨ ¬q] ↔ [p → (r ∧ ¬q)]: " << rownowaznosc((implikacja(p, r) || !q), implikacja(p, (r && !q))) << endl;
     cout << "g) [(¬r ∨ q) ∨ ¬(q ∧ r)] → [¬(q → p)]: " << implikacja((!r || q) || !(q && r), !implikacja(q, p)) << endl;
 }
+
 int main() {
+    // Przykład użycia
     bool p = true;
-    bool q = false;
-    bool r = true;
+    bool q = true;
+    bool r = false;
+
     ocena_zdan(p, q, r);
 
     return 0;
